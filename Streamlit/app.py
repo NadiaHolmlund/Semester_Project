fdd58b2c03d28cbdf6b5c1a0d2235ff37af31b2b
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2023-05-19 12:59:57 +0200

    test

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -1,14 +1,5 @@
 # PoC Streamlit application
 
-# Imports
 import streamlit as st
 
-pd.set_option("display.max_columns", None)
-
-# Setting up page configurations
-st.set_page_config(
-    page_title="Home",
-    page_icon="🏠",
-    layout="wide")
-
-st.title("Hello")
+"Hello, World!"
