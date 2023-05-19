69a3989a90db6b8d528a93bc84cb5db882703583
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2023-05-19 13:16:11 +0200

    page set

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -1,5 +1,20 @@
 # PoC Streamlit application
 
+# Imports
 import streamlit as st
 
-"Hello, World!"
+# Setting up page configurations
+st.set_page_config(
+    page_title="Home",
+    page_icon="💀",
+    layout="wide")
+
+st.title("Facial Emotion Recognition")
+
+# Loading images and videos only once
+#@st.experimental_singleton
+#def read_objects():
+
+#    return 
+
+# = read_objects()
