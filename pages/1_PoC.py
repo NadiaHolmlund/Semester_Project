75f8b9a007f53c0fedb6ed9260d8b7c0447899fe
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2023-05-20 23:11:13 +0200

    objects

--- pages/1_PoC.py
+++ pages/1_PoC.py
@@ -13,3 +13,28 @@ st.set_page_config(
     page_icon="💀",
     layout="wide")
 
+
+
+
+
+
+# Loading data, models, scalers, explainers, etc., only once
+@st.experimental_singleton
+def read_objects():
+    # Importing processor and model
+    processor = ViTImageProcessor.from_pretrained('google/vit-base-patch16-224')
+    model = ViTForImageClassification.from_pretrained('NadiaHolmlund/Semester_Project', num_labels= 7, ignore_mismatched_sizes=True)
+   
+    # Creating labels
+    emotion_id = [0, 1, 2, 3, 4, 5, 6]
+    emotion_label = ['Anger', 'Disgust', 'Fear', 'Happiness', 'Sadness', 'Surprise', 'Neutral']
+    id2label = {id: label for id, label in zip(emotion_id, emotion_label)}
+
+    img_angry = st.image('NadiaHolmlund/Semester_Project/pages/Streamlit_content/img_angry.jpg')
+    img_sad = st.image('NadiaHolmlund/Semester_Project/pages/Streamlit_content/img_sad.jpg')
+    img_happy = st.image('NadiaHolmlund/Semester_Project/pages/Streamlit_content/img_happy.jpg')
+
+    return processor, model, emotion_id, emotion_label, id2label, img_angry, img_sad, img_happy
+
+
+processor, model, emotion_id, emotion_label, id2label, img_angry, img_sad, img_happy = read_objects()
