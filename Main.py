3b1e79851cc1edf4173336bdc2a530324eecd293
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2023-05-20 19:00:51 +0200

    desc

--- Main.py
+++ Main.py
@@ -1,3 +1,5 @@
 import streamlit as st
 
-st.write("hello")
+st.write("Main page giving a brief explanation of the project and the streamlit")
+st.write("PoC illustrates that the model work and is able to make predictions")
+st.write("application illustrates how the model would output would be applied on a user interface")
