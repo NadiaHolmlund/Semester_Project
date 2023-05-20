d00c7b007d2c1388ea3d1fdc5477b8cb93207fcf
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2023-05-21 00:42:51 +0200

    plot

--- pages/1_PoC.py
+++ pages/1_PoC.py
@@ -135,3 +135,22 @@ with tab2:
         if img_file_buffer is not None:
             stock_classification, logits_values = classify_image(camera_classification)
             st.metric(label='Emotion', value=stock_classification)
+    
+    fig, ax = plt.subplots(figsize=(8, 6))
+    bars = ax.barh(emotion_label, logits_values, height=0.1)
+
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+
+    text_position = max(logits_values) + 0.05  # Define the fixed position for the text
+
+    for i, bar in enumerate(bars):
+        ax.text(text_position, bar.get_y() + bar.get_height() / 2,
+                f'{logits_values[i]*100:.2f}%', va='center', ha='right')
+
+    plt.xticks([])  # Hide the x-axis tick labels
+
+    # Display the plot using st.pyplot()
+    st.pyplot(fig)
