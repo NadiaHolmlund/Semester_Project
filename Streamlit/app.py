3b9bc69b5bcb523fc7d8335a98f0edb3dc6a717e
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2023-05-20 16:46:14 +0200

    model

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -18,3 +18,4 @@ url = 'https://c0.wallpaperflare.com/preview/990/418/320/adorable-black-and-whit
 image = Image.open(requests.get(url, stream=True).raw)
 
 processor = ViTImageProcessor.from_pretrained('google/vit-base-patch16-224')
+model = ViTForImageClassification.from_pretrained('NadiaHolmlund/Semester_Project', ignore_mismatched_sizes=True)
