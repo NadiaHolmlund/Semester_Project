1270309c0a051de8ff608e0ce853307683d3cf00
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2023-05-20 16:44:02 +0200

    s

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -2,9 +2,6 @@
 
 # Imports
 import streamlit as st
-from transformers import ViTImageProcessor, ViTForImageClassification
-from PIL import Image
-import requests
 
 # Setting up page configurations
 st.set_page_config(
@@ -16,16 +13,3 @@ st.title("Facial Emotion Recognition")
 
 
 
-url = 'https://c0.wallpaperflare.com/preview/990/418/320/adorable-black-and-white-black-and-white-boy.jpg'
-image = Image.open(requests.get(url, stream=True).raw)
-
-processor = ViTImageProcessor.from_pretrained('google/vit-base-patch16-224')
-model = ViTForImageClassification.from_pretrained('NadiaHolmlund/Semester_Project', ignore_mismatched_sizes=True)
-
-inputs = processor(images=image, return_tensors="pt")
-outputs = model(**inputs)
-logits = outputs.logits
-
-# model predicts one of the 1000 ImageNet classes
-predicted_class_idx = logits.argmax(-1).item()
-print("Predicted class:", model.config.id2label[predicted_class_idx])
