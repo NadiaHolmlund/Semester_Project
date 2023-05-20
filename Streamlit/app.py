bdd2dc25eefc1a4a88fb846c15daa93237fa60ca
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2023-05-20 18:25:31 +0200

    test

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -48,7 +48,22 @@ def classify_image(image):
 col1, col2 = st.columns(2)
 
 with col1:
-    camera_image = st.camera_input("Take a picture")
+    img_file_buffer = st.camera_input("Take a picture")
+
+    if img_file_buffer is not None:
+        # To read image file buffer as a PIL Image:
+        img = Image.open(img_file_buffer)
+
+        # To convert PIL Image to numpy array:
+        img_array = np.array(img)
+
+        # Check the type of img_array:
+        # Should output: <class 'numpy.ndarray'>
+        st.write(type(img_array))
+
+        # Check the shape of img_array:
+        # Should output shape: (height, width, channels)
+        st.write(img_array.shape)
     
 
 with col2:
