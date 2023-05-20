443ea87a251809bfc53819c5fe651dd45f1582ce
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2023-05-20 15:45:29 +0200

    test

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -2,6 +2,9 @@
 
 # Imports
 import streamlit as st
+from transformers import ViTImageProcessor, ViTForImageClassification
+from PIL import Image
+import requests
 
 # Setting up page configurations
 st.set_page_config(
@@ -12,13 +15,11 @@ st.set_page_config(
 st.title("Facial Emotion Recognition")
 
 
-from transformers import ViTImageProcessor, ViTForImageClassification
-from PIL import Image
-import requests
+
+
 
 url = 'https://c0.wallpaperflare.com/preview/990/418/320/adorable-black-and-white-black-and-white-boy.jpg'
 image = Image.open(requests.get(url, stream=True).raw)
-image
 
 processor = ViTImageProcessor.from_pretrained('google/vit-base-patch16-224')
 model = ViTForImageClassification.from_pretrained('/content/gdrive/MyDrive/Semester_Project/mlruns/355554225098101950/71e8bc2658ca43339a0f42befc39de4f/artifacts/Model_experiment_1', ignore_mismatched_sizes=True)
