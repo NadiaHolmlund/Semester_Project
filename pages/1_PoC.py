5b1126bceb8acab7e26f1937421d61bf9862f9e9
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2023-05-21 00:25:43 +0200

    plotly

--- pages/1_PoC.py
+++ pages/1_PoC.py
@@ -7,7 +7,7 @@ from PIL import Image
 import requests
 import torch
 import matplotlib.pyplot as plt
-import plotly
+import plotly.graph_objects as go
 
 # Setting up page configurations
 st.set_page_config(
@@ -96,7 +96,6 @@ col2.image(img_happiness)
 col3.image(img_sadness)
 
 
-import plotly.graph_objects as go
 
 layout = go.Layout(
     paper_bgcolor='rgba(0,0,0,0)',
