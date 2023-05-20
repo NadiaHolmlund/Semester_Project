e625713e7f2b40b3e6ebb2e5c23ebcce6c940e0c
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2023-05-20 15:13:04 +0200

    model

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -11,10 +11,19 @@ st.set_page_config(
 
 st.title("Facial Emotion Recognition")
 
-# Loading images and videos only once
-#@st.experimental_singleton
-#def read_objects():
+@st.cache
+def load_model():
 
-#    return 
+    save_dest = Path('model')
+    save_dest.mkdir(exist_ok=True)
+    
+    f_checkpoint = Path("/content/gdrive/MyDrive/Semester_Project/mlruns/355554225098101950/71e8bc2658ca43339a0f42befc39de4f/artifacts/Model_experiment_1")
 
-# = read_objects()
+    if not f_checkpoint.exists():
+        with st.spinner("Downloading model... this may take awhile! \n Don't stop it!"):
+            from GD_download import download_file_from_google_drive
+            download_file_from_google_drive(cloud_model_location, f_checkpoint)
+    
+    model = torch.load(f_checkpoint, map_location=device)
+    model.eval()
+    return model
