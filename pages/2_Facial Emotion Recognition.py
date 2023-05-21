bd55cdc2770a3a46d6dc2fba99cdde3f75b16ca1
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2023-05-21 19:42:11 +0200

    test

--- pages/2_Facial Emotion Recognition.py
+++ pages/2_Facial Emotion Recognition.py
@@ -88,8 +88,9 @@ with tab1:
         option = st.selectbox('', ('Select an Image', 'img_1', 'img_2', 'img_3'))
     
     with col2:
-        predicted_class, logits_values = predict_class(img_1)
-        st.metric(label='Emotion', value=predicted_class, )
+        st.write('')
+        #predicted_class, logits_values = predict_class(img_1)
+        #st.metric(label='Emotion', value=predicted_class, )
 
 with tab2:
     st.write('hello')
