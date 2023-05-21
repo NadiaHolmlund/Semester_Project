11b4bc484cc513075fe0bd69dbf4885675dca47a
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2023-05-21 03:09:26 +0200

    av

--- pages/2_MoodTrackr.py
+++ pages/2_MoodTrackr.py
@@ -31,11 +31,11 @@ img_nadia, img_nicklas, img_nikolaj = read_objects()
 
 
 with st.sidebar:
-    avatar = st.selectbox(
-        "Choose Your Avatar and Select a Timeframe",
-        ('Select from list', 'Nadia', 'Nicklas', 'Nikolaj'))
-
     timeframe = st.slider('', value=(time(11, 30), time(12, 45)))
+
+    avatar = st.selectbox(
+        "Select a Timeframe and Choose Your Avatar",
+        ('Select from list', 'Nadia', 'Nicklas', 'Nikolaj'))
 
     col1, col2, col3 = st. columns(3)
     col1.image(img_nadia, caption='Nadia')
