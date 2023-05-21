8c9446c87de749039bd77db3b00e08f3b154794e
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2023-05-21 02:35:13 +0200

    img

--- pages/2_MoodTrackr.py
+++ pages/2_MoodTrackr.py
@@ -19,13 +19,13 @@ avatar = st.radio(
 def read_objects():
     # Importing images
     nadia_url = 'https://github.com/NadiaHolmlund/Semester_Project/raw/main/Streamlit_content/img_anger.jpg'
-    img_nadia = Image.open(requests.get(img_anger_url, stream=True).raw)
+    img_nadia = Image.open(requests.get(nadia_url, stream=True).raw)
 
     nicklas_url = 'https://github.com/NadiaHolmlund/Semester_Project/raw/main/Streamlit_content/img_happiness.jpg'
-    img_nicklas = Image.open(requests.get(img_happiness_url, stream=True).raw)
+    img_nicklas = Image.open(requests.get(nicklas_url, stream=True).raw)
 
     nikolaj_url = 'https://github.com/NadiaHolmlund/Semester_Project/raw/main/Streamlit_content/img_sadness.jpg'
-    img_nikolaj = Image.open(requests.get(img_sadness_url, stream=True).raw)
+    img_nikolaj = Image.open(requests.get(nikolaj_url, stream=True).raw)
 
     return img_nadia, img_nicklas, img_nikolaj
 
