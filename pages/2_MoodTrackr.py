b54795dcc9e76ceb2ca52a4cf9ff3f3fe4d20a16
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2023-05-21 03:24:15 +0200

    txt

--- pages/2_MoodTrackr.py
+++ pages/2_MoodTrackr.py
@@ -48,4 +48,4 @@ if avatar == 'Select':
 
 else:    
     st.title("MoodTrackr")
-    st.write('Hi ' + avatar +'! So nice to have you back, let\' see how you are feeling today, shall we?')
+    st.write('Hi ' + avatar +'! I\'m so glad to see you today, let\' have a look you are feeling today, shall we?')
