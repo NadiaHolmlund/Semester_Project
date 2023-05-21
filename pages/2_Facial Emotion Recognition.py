6e3b426c4fcfb54afd1ef34f3ad38fa03f953117
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2023-05-21 18:22:01 +0200

    img

--- pages/2_Facial Emotion Recognition.py
+++ pages/2_Facial Emotion Recognition.py
@@ -24,14 +24,22 @@ def read_objects():
 
     # Importing images for PoC
     img_anger_url = 'https://github.com/NadiaHolmlund/Semester_Project/raw/main/Streamlit_content/img_anger.jpg'
-    img_disgust_url =
-    img_fear_url =
+    img_disgust_url = 'https://github.com/NadiaHolmlund/Semester_Project/blob/main/Streamlit_content/img_disgust.jpg'
+    img_fear_url = 'https://github.com/NadiaHolmlund/Semester_Project/blob/main/Streamlit_content/img_fear.jpg'
     img_happiness_url = 'https://github.com/NadiaHolmlund/Semester_Project/raw/main/Streamlit_content/img_happiness.jpg'
     img_sadness_url = 'https://github.com/NadiaHolmlund/Semester_Project/raw/main/Streamlit_content/img_sadness.jpg'
-    img_surprise_url =
-    img_neutral_url =
+    img_surprise_url = 'https://github.com/NadiaHolmlund/Semester_Project/blob/main/Streamlit_content/img_surprise.jpg'
+    img_neutral_url = 'https://github.com/NadiaHolmlund/Semester_Project/blob/main/Streamlit_content/img_neutral.jpg'
 
-    return processor, model, class_id, class_label, id2label
+    img_anger = Image.open(requests.get(img_anger_url, stream=True).raw)
+    img_disgust = Image.open(requests.get(img_disgust_url, stream=True).raw)
+    img_fear = Image.open(requests.get(img_fear_url, stream=True).raw)
+    img_happiness = Image.open(requests.get(img_happiness_url, stream=True).raw)
+    img_sadness = Image.open(requests.get(img_sadness_url, stream=True).raw)
+    img_surprise = Image.open(requests.get(img_surprise_url, stream=True).raw)
+    img_neutral = Image.open(requests.get(img_neutral_url, stream=True).raw)
 
-processor, model, class_id, class_label, id2label = read_objects()
+    return processor, model, class_id, class_label, id2label, img_anger, img_disgust, img_fear, img_happiness, img_sadness, img_surprise, img_neutral
+
+processor, model, class_id, class_label, id2label, img_anger, img_disgust, img_fear, img_happiness, img_sadness, img_surprise, img_neutral = read_objects()
 
