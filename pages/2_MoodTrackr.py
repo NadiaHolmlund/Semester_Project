e53adf4c47e02fbabe5d60ce8a3cf0741f599d90
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2023-05-21 02:33:24 +0200

    img

--- pages/2_MoodTrackr.py
+++ pages/2_MoodTrackr.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from PIL import Image
+import requests
 
 # Setting up page configurations
 st.set_page_config(
@@ -11,8 +13,22 @@ avatar = st.radio(
     ('Nadia', 'Nicklas', 'Nikolaj'))
 
 
-from PIL import Image
 
-image = Image.open('NadiaHolmlund/Semester_Project/Streamlit_content/img_anger.jpg')
+# Loading processor, model, labels and images only once
+@st.experimental_singleton
+def read_objects():
+    # Importing images
+    nadia_url = 'https://github.com/NadiaHolmlund/Semester_Project/raw/main/Streamlit_content/img_anger.jpg'
+    img_nadia = Image.open(requests.get(img_anger_url, stream=True).raw)
+
+    nicklas_url = 'https://github.com/NadiaHolmlund/Semester_Project/raw/main/Streamlit_content/img_happiness.jpg'
+    img_nicklas = Image.open(requests.get(img_happiness_url, stream=True).raw)
+
+    nikolaj_url = 'https://github.com/NadiaHolmlund/Semester_Project/raw/main/Streamlit_content/img_sadness.jpg'
+    img_nikolaj = Image.open(requests.get(img_sadness_url, stream=True).raw)
+
+    return img_nadia, img_nicklas, img_nikolaj
+
+img_nadia, img_nicklas, img_nikolaj = read_objects()
 
 st.image(image, caption='Sunrise by the mountains')
