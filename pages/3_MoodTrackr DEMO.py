2efbe1a73d7560b08ef2de77bfaa368561f3eef3
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2023-05-29 21:02:09 +0200

    time

--- pages/3_MoodTrackr DEMO.py
+++ pages/3_MoodTrackr DEMO.py
@@ -39,8 +39,8 @@ with st.sidebar:
     
     #timeframe = st.slider('Select a Timeframe', value=((7, 30), (16, 30)))
 
-    start_time = st.time_input('Select Start Time', datetime.time(7, 30))
-    end_time = st.time_input('Select End Time', datetime.time(16, 30))
+    start_time = st.time_input('Select Start Time', datetime(7, 30))
+    end_time = st.time_input('Select End Time', datetime(16, 30))
 
     if start_time < end_time:
         pass
