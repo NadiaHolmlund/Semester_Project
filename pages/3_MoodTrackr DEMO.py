a46bc6c6b48c98915264d70e9e928232d9418557
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2023-05-29 17:22:48 +0200

    plot

--- pages/3_MoodTrackr DEMO.py
+++ pages/3_MoodTrackr DEMO.py
@@ -62,9 +62,9 @@ else:
     # Assuming you have the 'nadia_df' DataFrame with columns: 'application', 'application_type', 'application_duration_min'
 
     fig = go.Figure(go.Sunburst(
-        labels=nadia_df['application'],
-        parents=nadia_df['application_type'],
-        values=nadia_df['application_duration_min'],
+        labels=['GitHub', 'Google Colab', 'ChatGPT', 'Google Colab', 'Stack Overflow', 'Facebook', 'TikTok'],
+        parents=['Code Collaboration', 'Coding Notebook', 'AI Chatbot', 'Coding Notebook', 'Programming Q&A', 'Social Media', 'Social Media'],
+        values=[30, 45, 15, 15, 15, 90, 150],
     ))
     fig.update_layout(margin=dict(t=0, l=0, r=0, b=0))
 
