8a207879eed530cc97bdf1ef5bf0e456998d1af3
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2023-05-29 11:03:23 +0200

    img

--- pages/1_Facial Emotion Recognition.py
+++ pages/1_Facial Emotion Recognition.py
@@ -70,4 +70,5 @@ tab1, tab2 = st.tabs(['Images', 'Camera'])
 with tab1:
 
     col1, col2, col3, col4, col5, col6, col7, col8, col9 = st.columns(9)
-        col1.image(img_1_sq, 'Image 1')
+    
+    col1.image(img_1_sq, 'Image 1')
