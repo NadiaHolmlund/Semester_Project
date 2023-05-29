aea724160741e40c696efda52a9afee5fb31e21d
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2023-05-29 11:08:11 +0200

    img

--- pages/1_Facial Emotion Recognition.py
+++ pages/1_Facial Emotion Recognition.py
@@ -24,30 +24,31 @@ def read_objects():
     class_label = ['Anger', 'Disgust', 'Fear', 'Happiness', 'Sadness', 'Surprise', 'Neutral']
     id2label = {id: label for id, label in zip(class_id, class_label)}
 
+    # Loading images with rounded edges
+    img_1_rd = Image.open(requests.get('https://github.com/NadiaHolmlund/Semester_Project/raw/main/Streamlit_content/rounded_images/img_1.jpg', stream=True).raw)
+    img_2_rd = Image.open(requests.get('https://github.com/NadiaHolmlund/Semester_Project/raw/main/Streamlit_content/rounded_images/img_2.jpg', stream=True).raw)
+    img_3_rd = Image.open(requests.get('https://github.com/NadiaHolmlund/Semester_Project/raw/main/Streamlit_content/rounded_images/img_3.jpg', stream=True).raw)
+    img_4_rd = Image.open(requests.get('https://github.com/NadiaHolmlund/Semester_Project/raw/main/Streamlit_content/rounded_images/img_4.jpg', stream=True).raw)
+    img_5_rd = Image.open(requests.get('https://github.com/NadiaHolmlund/Semester_Project/raw/main/Streamlit_content/rounded_images/img_5.jpg', stream=True).raw)
+    img_6_rd = Image.open(requests.get('https://github.com/NadiaHolmlund/Semester_Project/raw/main/Streamlit_content/rounded_images/img_6.jpg', stream=True).raw)
+    img_7_rd = Image.open(requests.get('https://github.com/NadiaHolmlund/Semester_Project/raw/main/Streamlit_content/rounded_images/img_7.jpg', stream=True).raw)
+    img_8_rd = Image.open(requests.get('https://github.com/NadiaHolmlund/Semester_Project/raw/main/Streamlit_content/rounded_images/img_8.jpg', stream=True).raw)
+    img_9_rd = Image.open(requests.get('https://github.com/NadiaHolmlund/Semester_Project/raw/main/Streamlit_content/rounded_images/img_9.jpg', stream=True).raw)
+
     # Loading images with square edges
-#    img_1_sqr_url = 'https://github.com/NadiaHolmlund/Semester_Project/raw/main/Streamlit_content/square_images/img_1.jpg'
-#    img_2_sqr_url = 'https://github.com/NadiaHolmlund/Semester_Project/raw/main/Streamlit_content/square_images/img_2.jpg'
-#    img_3_sqr_url = 'https://github.com/NadiaHolmlund/Semester_Project/raw/main/Streamlit_content/img_3.jpg'
-#    img_4_sqr_url = 'https://github.com/NadiaHolmlund/Semester_Project/raw/main/Streamlit_content/img_4.jpg'
-#    img_5_sqr_url = 'https://github.com/NadiaHolmlund/Semester_Project/raw/main/Streamlit_content/img_5.jpg'
-#    img_6_sqr_url = 'https://github.com/NadiaHolmlund/Semester_Project/raw/main/Streamlit_content/img_6.jpg'
-#    img_7_sqr_url = 'https://github.com/NadiaHolmlund/Semester_Project/raw/main/Streamlit_content/img_7.jpg'
-#    img_8_sqr_url = 'https://github.com/NadiaHolmlund/Semester_Project/raw/main/Streamlit_content/img_8.jpg'
-#    img_9_sqr_url = 'https://github.com/NadiaHolmlund/Semester_Project/raw/main/Streamlit_content/img_9.jpg'
-
     img_1_sq = Image.open(requests.get('https://github.com/NadiaHolmlund/Semester_Project/raw/main/Streamlit_content/square_images/img_1.jpg', stream=True).raw)
-#    img_2_sqr = Image.open(requests.get(img_2, stream=True).raw)
-#    img_3_sqr = Image.open(requests.get(img_3, stream=True).raw)
-#    img_4_sqr = Image.open(requests.get(img_4, stream=True).raw)
-#    img_5_sqr = Image.open(requests.get(img_5, stream=True).raw)
-#    img_6_sqr = Image.open(requests.get(img_6, stream=True).raw)
-#    img_7_sqr = Image.open(requests.get(img_7, stream=True).raw)
-#    img_8_sqr = Image.open(requests.get(img_8, stream=True).raw)
-#    img_9_sqr = Image.open(requests.get(img_9, stream=True).raw)
+    img_2_sq = Image.open(requests.get('https://github.com/NadiaHolmlund/Semester_Project/raw/main/Streamlit_content/square_images/img_2.jpg', stream=True).raw)
+    img_3_sq = Image.open(requests.get('https://github.com/NadiaHolmlund/Semester_Project/raw/main/Streamlit_content/square_images/img_3.jpg', stream=True).raw)
+    img_4_sq = Image.open(requests.get('https://github.com/NadiaHolmlund/Semester_Project/raw/main/Streamlit_content/square_images/img_4.jpg', stream=True).raw)
+    img_5_sq = Image.open(requests.get('https://github.com/NadiaHolmlund/Semester_Project/raw/main/Streamlit_content/square_images/img_5.jpg', stream=True).raw)
+    img_6_sq = Image.open(requests.get('https://github.com/NadiaHolmlund/Semester_Project/raw/main/Streamlit_content/square_images/img_6.jpg', stream=True).raw)
+    img_7_sq = Image.open(requests.get('https://github.com/NadiaHolmlund/Semester_Project/raw/main/Streamlit_content/square_images/img_7.jpg', stream=True).raw)
+    img_8_sq = Image.open(requests.get('https://github.com/NadiaHolmlund/Semester_Project/raw/main/Streamlit_content/square_images/img_8.jpg', stream=True).raw)
+    img_9_sq = Image.open(requests.get('https://github.com/NadiaHolmlund/Semester_Project/raw/main/Streamlit_content/square_images/img_9.jpg', stream=True).raw)
 
-    return processor, model, class_id, class_label, id2label, img_1_sq
+    return processor, model, class_id, class_label, id2label, img_1_rd, img_2_rd, img_3_rd, img_4_rd, img_5_rd, img_6_rd, img_7_rd, img_8_rd, img_9_rd, img_1_sq, img_2_sq, img_3_sq, img_4_sq, img_5_sq, img_6_sq, img_7_sq, img_8_sq, img_9_sq
 
-processor, model, class_id, class_label, id2label, img_1_sq = read_objects()
+processor, model, class_id, class_label, id2label, img_1_rd, img_2_rd, img_3_rd, img_4_rd, img_5_rd, img_6_rd, img_7_rd, img_8_rd, img_9_rd, img_1_sq, img_2_sq, img_3_sq, img_4_sq, img_5_sq, img_6_sq, img_7_sq, img_8_sq, img_9_sq = read_objects()
 
 
 # Defining a function to predict the class of emotion
@@ -71,4 +72,12 @@ with tab1:
 
     col1, col2, col3, col4, col5, col6, col7, col8, col9 = st.columns(9)
     
-    col1.image(img_1_sq, 'Image 1')
+    col1.image(img_1_rd, 'Image 1')
+    col2.image(img_2_rd, 'Image 2')
+    col3.image(img_3_rd, 'Image 3')
+    col4.image(img_4_rd, 'Image 4')
+    col5.image(img_5_rd, 'Image 5')
+    col6.image(img_6_rd, 'Image 6')
+    col7.image(img_7_rd, 'Image 7')
+    col8.image(img_8_rd, 'Image 8')
+    col9.image(img_9_rd, 'Image 9')
