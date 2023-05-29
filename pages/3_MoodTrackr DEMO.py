dbb76ecabfaeea19aa12215f8a62325d78910a83
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2023-05-29 17:17:17 +0200

    plot

--- pages/3_MoodTrackr DEMO.py
+++ pages/3_MoodTrackr DEMO.py
@@ -49,7 +49,7 @@ else:
 
     fig =go.Figure(go.Sunburst(
         labels=["Eve", "Cain", "Seth", "Enos", "Noam", "Abel", "Awan", "Enoch", "Azura"],
-        parents=["", "Mood", "Eve", "Seth", "Seth", "Eve", "Eve", "Awan", "Eve" ],
+        parents=["", "Eve", "Eve", "Seth", "Seth", "Eve", "Eve", "Awan", "Eve" ],
         values=[10, 14, 12, 10, 2, 6, 6, 4, 4],
     ))
     fig.update_layout(margin = dict(t=0, l=0, r=0, b=0))
