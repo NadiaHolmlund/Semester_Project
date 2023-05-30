05f1df9cd53370676f369ee8aaf180ce83bc41f7
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2023-05-30 22:34:56 +0200

    col names

--- pages/2_MoodTrackr DEMO.py
+++ pages/2_MoodTrackr DEMO.py
@@ -130,12 +130,12 @@ else:
         fig = px.sunburst(
             data_frame=timeframe_df,
             path=['user_name', 'website_cat', 'website', 'class_label'],
-            values='application_duration_min',
+            values='website_cat',
             color_discrete_sequence=['#0E1117'],
             hover_data={'user_name':False,
-                        'application_type':False,
-                        'application':False,
-                        'application_duration_min':False})
+                        'website_cat':False,
+                        'website':False,
+                        'website_cat':False})
 
         # Updating plot layout
         fig.update_layout(
