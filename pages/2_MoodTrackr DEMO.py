45bee073d14013c7ba704c424cc8fb10ffef7719
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2023-05-30 23:07:07 +0200

    hover

--- pages/2_MoodTrackr DEMO.py
+++ pages/2_MoodTrackr DEMO.py
@@ -136,7 +136,7 @@ else:
                 path=['user_name', 'website_cat', 'website', 'class_label'],
                 values='website_dur_min',
                 color_discrete_sequence=['#0E1117'],
-                hover_data={'website_dur_min':'duration'})
+                hover_data={'website_dur_min':False})
 
             # Updating plot layout
             fig.update_layout(
