d11322b9fc3af66ee59b51e2d9dbbc7aa3d0c943
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2023-05-30 10:37:14 +0200

    fig

--- pages/3_MoodTrackr DEMO.py
+++ pages/3_MoodTrackr DEMO.py
@@ -5,6 +5,8 @@ import requests
 from datetime import time
 import plotly.express as px
 import pandas as pd
+import plotly.graph_objects as go
+import numpy as np
 
 # Setting up page configurations
 st.set_page_config(
@@ -78,108 +80,63 @@ else:
         st.plotly_chart(fig, use_container_width=True)
 
     with col2:
-        import plotly.graph_objects as go
-
-        class_label = ['Anger', 'Disgust', 'Fear', 'Happiness', 'Sadness', 'Surprise', 'Neutral']
+        plot_bgcolor = "#262730"
+        quadrant_colors = [plot_bgcolor, "#f25829", "#f2a529", "#eff229", "#85e043", "#2bad4e"]
+        class_labels = ['Anger', 'Disgust', 'Fear', 'Happiness', 'Sadness', 'Surprise', 'Neutral']
         smileys = ['😡', '🤢', '😨', '😄', '😢', '😮', '😐']
-        counts = [10, 5, 7, 15, 8, 12, 20]  # Replace with your actual counts data
+        n_quadrants = len(quadrant_colors) - 1
 
-        # Find the index of the class with the highest count
-        max_count_index = counts.index(max(counts))
+        current_value = 19
+        min_value = 0
+        max_value = 50
+        hand_length = np.sqrt(2) / 6  # Adjust the hand length as desired
+        hand_angle = np.pi * (1 - (max(min_value, min(max_value, current_value)) - min_value) / (max_value - min_value))
 
-        # Create the gauge chart
-        fig = go.Figure(go.Indicator(
-            mode="gauge+number",
-            value=max_count_index,
-            gauge={
-                'axis': {'range': [None, len(class_label) - 1], 'ticktext': smileys, 'tickvals': list(range(len(class_label)))},
-                'bar': {'color': 'gray', 'thickness': 0.4},
-                'steps': [
-                    {'range': [0, len(class_label) - 1], 'color': 'lightgray'}
+        fig = go.Figure(
+            data=[
+                go.Pie(
+                    values=[1 / 7] * 7,
+                    rotation=90,
+                    hole=0.5,
+                    marker_colors=quadrant_colors,
+                    text=smileys,
+                    textinfo="text",
+                    hoverinfo="skip",
+                    textfont=dict(size=40)  # Adjust the text size as desired
+                ),
+            ],
+            layout=go.Layout(
+                showlegend=False,
+                margin=dict(b=0, t=10, l=10, r=10),
+                width=450,
+                height=450,
+                paper_bgcolor=plot_bgcolor,
+                annotations=[
+                    go.layout.Annotation(
+                        text=f"<b>Dominant Mood</b>",
+                        x=0.5, xanchor="center", xref="paper",
+                        y=0.25, yanchor="bottom", yref="paper",
+                        showarrow=False,
+                    )
                 ],
-                'threshold': {
-                    'line': {'color': 'red', 'width': 3},
-                    'thickness': 0.75,
-                    'value': max_count_index
-                },
-            },
-        ))
-
-        # Set the smileys as annotations with adjusted font size
-        fig.add_annotation(
-            x=0.5,
-            y=0.5,
-            text=smileys[max_count_index],
-            font={'size': 100},  # Adjust the font size for smileys
-            showarrow=False,
+                shapes=[
+                    go.layout.Shape(
+                        type="circle",
+                        x0=0.48, x1=0.52,
+                        y0=0.48, y1=0.52,
+                        fillcolor="#333",
+                        line_color="#333",
+                    ),
+                    go.layout.Shape(
+                        type="line",
+                        x0=0.5, x1=0.5 + hand_length * np.cos(hand_angle),
+                        y0=0.5, y1=0.5 + hand_length * np.sin(hand_angle),
+                        line=dict(color="#333", width=4)
+                    )
+                ]
+            )
         )
-
 
         # Display the chart
         st.plotly_chart(fig, use_container_width=True)
 
-
-
-
-import plotly.graph_objects as go
-import numpy as np
-
-plot_bgcolor = "#262730"
-quadrant_colors = [plot_bgcolor, "#f25829", "#f2a529", "#eff229", "#85e043", "#2bad4e"]
-class_labels = ['Anger', 'Disgust', 'Fear', 'Happiness', 'Sadness', 'Surprise', 'Neutral']
-smileys = ['😡', '🤢', '😨', '😄', '😢', '😮', '😐']
-n_quadrants = len(quadrant_colors) - 1
-
-current_value = 19
-min_value = 0
-max_value = 50
-hand_length = np.sqrt(2) / 6  # Adjust the hand length as desired
-hand_angle = np.pi * (1 - (max(min_value, min(max_value, current_value)) - min_value) / (max_value - min_value))
-
-fig = go.Figure(
-    data=[
-        go.Pie(
-            values=[1 / 7] * 7,
-            rotation=90,
-            hole=0.5,
-            marker_colors=quadrant_colors,
-            text=smileys,
-            textinfo="text",
-            hoverinfo="skip",
-            textfont=dict(size=40)  # Adjust the text size as desired
-        ),
-    ],
-    layout=go.Layout(
-        showlegend=False,
-        margin=dict(b=0, t=10, l=10, r=10),
-        width=450,
-        height=450,
-        paper_bgcolor=plot_bgcolor,
-        annotations=[
-            go.layout.Annotation(
-                text=f"<b>Dominant Mood</b>",
-                x=0.5, xanchor="center", xref="paper",
-                y=0.25, yanchor="bottom", yref="paper",
-                showarrow=False,
-            )
-        ],
-        shapes=[
-            go.layout.Shape(
-                type="circle",
-                x0=0.48, x1=0.52,
-                y0=0.48, y1=0.52,
-                fillcolor="#333",
-                line_color="#333",
-            ),
-            go.layout.Shape(
-                type="line",
-                x0=0.5, x1=0.5 + hand_length * np.cos(hand_angle),
-                y0=0.5, y1=0.5 + hand_length * np.sin(hand_angle),
-                line=dict(color="#333", width=4)
-            )
-        ]
-    )
-)
-
- # Display the chart
-st.plotly_chart(fig, use_container_width=True)
