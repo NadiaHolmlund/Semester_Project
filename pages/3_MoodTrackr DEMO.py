478c428b72f9e7bee30a6d5cf8bdfb2e9dd3b589
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2023-05-30 12:01:02 +0200

    fig

--- pages/3_MoodTrackr DEMO.py
+++ pages/3_MoodTrackr DEMO.py
@@ -118,7 +118,8 @@ else:
             path=['user_name', 'application_type', 'application', 'class_label'],
             values='application_duration_min',
             hover_data={'class_label': False},
-            color_discrete_sequence=['#0E1117'])
+            color_discrete_sequence=['#0E1117'],
+            hoverinfo="skip")
 
         # Updating plot layout
         fig.update_layout(
