f868f9d3b3cae6a5b751fb5f85a08a2aaddec666
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2023-05-30 14:19:25 +0200

    metr

--- pages/3_MoodTrackr DEMO.py
+++ pages/3_MoodTrackr DEMO.py
@@ -155,9 +155,8 @@ col4.metric(label="Duration: 15 min.", value="Facebook", delta="Increases Surpri
 
 
 # Assuming your dataframe is named "df"
-grouped_df = avatar_df.groupby('application').agg({'application_duration_min': 'sum', 'class_label': lambda x: x.mode()[0]}).reset_index()
+grouped_df = avatar_df.groupby('application').agg({'application_duration_min': 'sum', 'class_label': lambda x: x.mode()[0]}).reset_index().sort_values('application_duration_min', ascending=False)
 
-# Sort the dataframe by 'application_duration_min'
-grouped_df_sorted = grouped_df.sort_values('application_duration_min', inplace=True)
 
-st.metric(label="Duration: " + str(grouped_df_sorted.iloc[0]['application_duration_min']), value=str(grouped_df_sorted.iloc[0]['application']), delta="Increases Happiness")
+
+st.metric(label="Duration: " + str(grouped_df.iloc[0]['application_duration_min']), value=str(grouped_df.iloc[0]['application']), delta="Increases Happiness")
