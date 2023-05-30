fb7284766d4ded110e463a7674c6d260db6db5b8
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2023-05-30 23:48:45 +0200

    txt

--- About_This_Page.py
+++ About_This_Page.py
@@ -18,6 +18,7 @@ img_fer, img_moodtrackr = read_objects()
 col1, col2 = st.columns(2)
 
 with col1:
+    st.write('')
     st.subheader('Facial Emotion Recognition')
     st.write("The page 'Facial Emotion Recognition' allows stakeholders to interact with the fine-tuned model, thus observing how the model classifies images and webcam snapshots in real-time.")
     st.write("The page includes two tabs, namely 'Images' and 'Camera', in which emotions are classified and displayed along with the probability of each class labelling.")  
@@ -32,4 +33,5 @@ with col1:
     st.image(img_moodtrackr)
 
 with col2:
+    st.write('')
     st.subheader('MoodTrackr DEMO')
