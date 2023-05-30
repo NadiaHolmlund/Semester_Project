b03e85855e3e73b2cb310476ea85fb630017b9b5
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2023-05-30 23:21:00 +0200

    img

--- About_This_Page.py
+++ About_This_Page.py
@@ -1,9 +1,20 @@
+# Imports
 import streamlit as st
+from PIL import Image
+import requests
 
-st.write("Main page giving a brief explanation of the project and the streamlit")
-st.write("PoC illustrates that the model work and is able to make predictions")
-st.write("application illustrates how the model would output would be applied on a user interface")
+# Loading the images only once
+@st.cache_resource
+def read_objects():
+    # Importing images
+    img_fer = Image.open(requests.get('https://github.com/NadiaHolmlund/Semester_Project/raw/main/Streamlit_content/rounded_images/img_1.png', stream=True).raw)
+    img_moodtrackr = Image.open(requests.get('https://github.com/NadiaHolmlund/Semester_Project/raw/main/Streamlit_content/rounded_images/img_2.png', stream=True).raw)
 
+    return img_fer, img_moodtrackr
+
+img_fer, img_moodtrackr = read_objects()
+
+# Defining the columns and adding descriptions
 col1, col2 = st.columns(2)
 
 with col1:
