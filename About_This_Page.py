88cc75f567eff56b4c85ff4290f39b576810d293
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2023-05-30 23:50:37 +0200

    txt

--- About_This_Page.py
+++ About_This_Page.py
@@ -19,7 +19,6 @@ col1, col2 = st.columns(2)
 
 with col1:
     st.write('')
-    st.write('')
     st.subheader('Facial Emotion Recognition')
     st.write("The page 'Facial Emotion Recognition' allows stakeholders to interact with the fine-tuned model, thus observing how the model classifies images and webcam snapshots in real-time.")
     st.write("The page includes two tabs, namely 'Images' and 'Camera', in which emotions are classified and displayed along with the probability of each class labelling.")  
@@ -35,5 +34,6 @@ with col1:
 
 with col2:
     st.write('')
-    st.write('')
     st.subheader('MoodTrackr DEMO')
+    st.write("MoodTrackr DEMO showcases the envisioned appearance of the dashboard which is to be presented to the end user when engaging with the product.")
+    st.write("The dashboard presents three informative elements: Today’s Overall Mood, a sunburst graph displaying the composition and structure of websites used and the emotions classified, as well as metrics displaying whether a website has a positive or negative impact on user mental health.")
