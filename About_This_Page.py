e2844e4491edfe51b4ae31133e76339cc9adb160
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2023-05-31 10:49:46 +0200

    img

--- About_This_Page.py
+++ About_This_Page.py
@@ -7,24 +7,27 @@ import requests
 @st.cache_resource
 def read_objects():
     # Importing images
-    img_fer = Image.open(requests.get('https://github.com/NadiaHolmlund/Semester_Project/raw/main/Streamlit_content/square_images/img_fer.png', stream=True).raw)
-    img_moodtrackr = Image.open(requests.get('https://github.com/NadiaHolmlund/Semester_Project/raw/main/Streamlit_content/square_images/img_moodtrackr.png', stream=True).raw)
+    img_default = Image.open(requests.get('https://github.com/NadiaHolmlund/Semester_Project/raw/main/Streamlit_content/rounded_images/img_default.png', stream=True).raw)
+    img_fer = Image.open(requests.get('https://github.com/NadiaHolmlund/Semester_Project/raw/main/Streamlit_content/rounded_images/img_fer.png', stream=True).raw)
+    img_moodtrackr = Image.open(requests.get('https://github.com/NadiaHolmlund/Semester_Project/raw/main/Streamlit_content/rounded_images/img_moodtrackr.png', stream=True).raw)
 
-    return img_fer, img_moodtrackr
+    return img_default, img_fer, img_moodtrackr
 
-img_fer, img_moodtrackr = read_objects()
+img_default, img_fer, img_moodtrackr = read_objects()
 
-# Defining columns and adding descriptions
+
+# Defining the introduction columns
 col1, col2 = st.columns(2)
 with col1:
     st.subheader('Welcome to this page')
     st.write('This is a PoC for the project bla bla bla')
 
 with col2:
-    st.write('maybe general picture of faces etc. - AI ish')
+    st.image(img_default)
 
+
+# Defining the Facial Emotion Recognition columns
 col1, col2 = st.columns(2)
-
 with col1:
     st.image(img_fer)
 
@@ -34,8 +37,8 @@ with col2:
     st.write("The page includes two tabs, namely 'Images' and 'Camera', in which emotions are classified and displayed along with the probability of each class labelling.")  
 
 
+# Defining the MoodTrackr columns
 col1, col2 = st.columns(2)
-
 with col1:
     st.subheader('MoodTrackr DEMO')
     st.write("MoodTrackr DEMO showcases the envisioned appearance of the dashboard which is to be presented to the end user when engaging with the product.")
